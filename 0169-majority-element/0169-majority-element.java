6c2880e7cef1785031af76f72abd546c0981ad06
bhagyajcse <bhagyaj.cse24@cmrit.ac.in>
2026-09-20 11:29:15 +0530

    Time: 2 ms (71.46%), Space: 63.1 MB (5.14%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,35 @@
+//BRUTE FOR APPROACH
+/*import java.util.*;
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<nums.length;i++){
+            int freq=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    freq++;
+                }
+            }
+            if(freq>n/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}*/
+class Solution {
+    public int majorityElement(int[] nums) {
+        int freq=0,ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(ans==nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+}
